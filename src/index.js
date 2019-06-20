3fdb71dd118da7c60398be6454cd94de1f07660f
Corey Snyder <corey.snyder@aver.io>
2019-06-19 21:50:25 -0400

    Implements Prisma client for saving data to database instead of in a var in memory.

--- src/index.js
+++ src/index.js
@@ -1,57 +1,59 @@
 const { GraphQLServer } = require('graphql-yoga')
 const _ = require('underscore')
-
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
-
-let idCount = links.length
+const { prisma } = require('./generated/prisma-client')
 
 const resolvers = {
   Query: {
     info: () => 'A clone of Hackernews',
-    feed: () => links,
-    link: (parent, args) => {
-      let linkFound;
-      _.each(links, (link) => {
-        if(link.id === args.id){
-          linkFound = link;
-        }
-      })
-
-      return (linkFound) ? linkFound : {}
-    }
+    feed: (root, args, context, info) => {
+      return context.prisma.links()
+    },
+    // link: (parent, args) => {
+    //   let linkFound;
+    //   _.each(links, (link) => {
+    //     if(link.id === args.id){
+    //       linkFound = link;
+    //     }
+    //   })
+    //   return (linkFound) ? linkFound : {}
+    // }
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
+    post: (root, args, context) => {
+      return context.prisma.createLink({
         url: args.url,
-      }
-      links.push(link)
-      return link
+        description: args.description,
+      })
     },
-    updateLink: (parent, {id, url, description}) => {
-      let found = _.findWhere(links, {id});
-      if(found){
-        found.url = url;
-        found.description = description;
-        return found
-      }
-    },
-    deleteLink: (parent, {id}) => {
-      links = _.reject(links, (link)=> link.id===id)
-    }
-
-  }
+  },
+  // Mutation: {
+  //   post: (parent, args) => {
+  //     const link = {
+  //       id: `link-${idCount++}`,
+  //       description: args.description,
+  //       url: args.url,
+  //     }
+  //     links.push(link)
+  //     return link
+  //   },
+  //   updateLink: (parent, {id, url, description}) => {
+  //     let found = _.findWhere(links, {id});
+  //     if(found){
+  //       found.url = url;
+  //       found.description = description;
+  //       return found
+  //     }
+  //   },
+  //   deleteLink: (parent, {id}) => {
+  //     links = _.reject(links, (link)=> link.id===id)
+  //   }
+  // }
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: { prisma },
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
