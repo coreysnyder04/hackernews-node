fe82fdf1bf3886c35d7333c5bf7460cbbcd2057a
Corey Snyder <corey.snyder@aver.io>
2019-06-20 10:12:50 -0400

    Refactor result of feed query to return a list of links as well as a count

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -13,7 +13,17 @@ async function feed(parent, args, context, info) {
     orderBy: args.orderBy
   })
 
-  return links
+  const count = await context.prisma
+    .linksConnection({
+      where,
+    })
+    .aggregate()
+    .count()
+
+  return {
+    links,
+    count,
+  }
 }
 
 function info(){
